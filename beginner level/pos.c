0f2d668a499ec2a7a94d1398622421a71489efe9
Mageshwara <37831118+Mageshwara@users.noreply.github.com>
2018-03-27 14:52:06 +0530

    Create pos.c

---
+++ beginner level/pos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number to check:");
+scanf("%d",&num);
+if(num<0)
+printf("\n %d is negative",num);
+else if(num>0)
+printf("\n %d is positive",num);
+else
+printf("\n the entered number is %d",num);
+return 0;
+}
+
+© 2018 GitHub, Inc.
